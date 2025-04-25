3f8a0faa488fff06ecafe96e6d7cb67b6eb52ca5
ChristopherRussell <19709603+ChristopherRussell@users.noreply.github.com>
2025-04-25 14:45:44 +0100

    fix custom shift

--- Dq9r0/i18n.h
+++ Dq9r0/i18n.h
@@ -4,3 +4,5 @@
 #define KC_PC_CUT LCTL(KC_X)
 #define KC_PC_COPY LCTL(KC_C)
 #define KC_PC_PASTE LCTL(KC_V)
+
+#define UK_POUND ALGR(KC_3)
--- Dq9r0/rules.mk
+++ Dq9r0/rules.mk
@@ -3,8 +3,10 @@ COMMAND_ENABLE = no
 ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
 SPACE_CADET_ENABLE = no
+REPEAT_KEY_ENABLE = yes 
 
 # Non-ORYX Custom QMK Features
 KEY_OVERRIDE_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
 
 SRC += features/custom_shift_keys.c
